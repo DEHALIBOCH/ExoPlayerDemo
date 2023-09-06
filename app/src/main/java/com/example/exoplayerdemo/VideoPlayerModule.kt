1058911550fac66831c2abf3860b070f04ebeab4
Marat <almabekov.marat@gmail.com>
2023-09-06 16:32:08 +0600

    Удаление не используемого импорта

--- app/src/main/java/com/example/exoplayerdemo/VideoPlayerModule.kt
+++ app/src/main/java/com/example/exoplayerdemo/VideoPlayerModule.kt
@@ -8,7 +8,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
-import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(ViewModelComponent::class)
